f5ad24df5d900651025a9db8e9b284f9dccb4f28
Hidehiro Nagaoka <kuinige@gmail.com>
2018-05-31 14:54:46 +0900

    Added vertical row and player1

--- src/Square.js
+++ src/Square.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './Square.css';
 
 function Square(props) {
+  const squareClass = props.number < 9 ? 'square-first-row' : 'square'
+  const playerClass = 'player1'
   return (
-    <div className="square" onClick={() => props.onClick()}>
-
+    <div className={squareClass} onClick={() => props.onClick()}>
+      <div className={playerClass}></div>
     </div>
   );
 }
